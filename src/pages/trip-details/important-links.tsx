31050bd7adb7b7f0c5c670e487ee342f6d6876fd
Rayanne Ramos <rayanneramosoficial@gmail.com>
2024-07-17 17:08:04 -0300

    :sparkles: feat: searching for api links

--- src/pages/trip-details/important-links.tsx
+++ src/pages/trip-details/important-links.tsx
@@ -1,10 +1,26 @@
 import { Link2, Plus } from "lucide-react";
 import { Button } from "../../components/button";
 import { RegisterLinkModal } from "./register-link-modal";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { api } from "../../lib/axios";
+
+interface ImportantLinkProps {
+  id: string;
+  title: string;
+  url: string;
+}
 
 export function ImportantLinks() {
   const [isModalRegisterLink, setIsModalRegisterLink] = useState(false);
+  const [links, setLinks] = useState<ImportantLinkProps[]>([]);
+  const { tripId } = useParams();
+
+  useEffect(() => {
+    api
+      .get(`/trips/${tripId}/links`)
+      .then((response) => setLinks(response.data.links));
+  }, [tripId]);
 
   function openModalRegisterLink() {
     setIsModalRegisterLink(true);
@@ -18,34 +34,27 @@ export function ImportantLinks() {
     <div className="space-y-6">
       <h2 className="font-semibold text-xl">Links importantes</h2>
       <div className="space-y-5">
-        <div className="flex items-center justify-between gap-4">
-          <div className="space-y-1.5">
-            <span className="block font-medium text-zinc-100">
-              Reserva do AirBnB
-            </span>
-            <a
-              href="#"
-              className="block text-xs text-zinc-400 truncate hover:text-zinc-200"
+        {links.map((link) => {
+          return (
+            <div
+              className="flex items-center justify-between gap-4"
+              key={link.id}
             >
-              https://www.airbnb.com.br/rooms/104700011
-            </a>
-          </div>
-          <Link2 className="text-zinc-400 size-5 shrink-0" />
-        </div>
-        <div className="flex items-center justify-between gap-4">
-          <div className="space-y-1.5">
-            <span className="block font-medium text-zinc-100">
-              Reserva do AirBnB
-            </span>
-            <a
-              href="#"
-              className="block text-xs text-zinc-400 truncate hover:text-zinc-200"
-            >
-              https://www.airbnb.com.br/rooms/104700011
-            </a>
-          </div>
-          <Link2 className="text-zinc-400 size-5 shrink-0" />
-        </div>
+              <div className="space-y-1.5">
+                <span className="block font-medium text-zinc-100">
+                  {link.title}
+                </span>
+                <a
+                  href="#"
+                  className="block text-xs text-zinc-400 truncate hover:text-zinc-200"
+                >
+                  {link.url}
+                </a>
+              </div>
+              <Link2 className="text-zinc-400 size-5 shrink-0" />
+            </div>
+          );
+        })}
       </div>
       <Button variant="secondary" size="full" onClick={openModalRegisterLink}>
         <Plus className="size-5" />
